cbdc2c51681de83b794f755c2edd80038191fea2
izreit <737703+izreit@users.noreply.github.com>
2024-07-29 00:34:03 +0900

    chore(*): allow partial build for debug

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -2,6 +2,9 @@ import { execSync } from "node:child_process";
 import { dirname, join, relative } from "node:path";
 import { fileURLToPath } from "node:url";
 
+const { FICCO_BUILD } = process.env;
+const mode = /^normal|full|all$/.test(FICCO_BUILD) ? FICCO_BUILD : "all";
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const root = relative(process.cwd(), join(__dirname, ".."));
 
@@ -15,11 +18,15 @@ function run(s) {
 }
 
 run(`npx vite build -- -ct upwind-preset`);
-run(`npx vite build -- -t reactive-full`);
-run(`npx vite build -- -t html-full`);
-run(`npx vite build -- -t whole-full`);
-run(`npx vite build -- -t reactive`);
-run(`npx vite build -- -t html`);
-run(`npx vite build -- -t whole`);
-run(`npx tsc -p ${path("tsconfig.build.json ")} --emitDeclarationOnly --outDir ${path("dist/full")}`);
-run(`npx tsc -p ${path("tsconfig.build.json ")} --emitDeclarationOnly --outDir ${path("dist/normal")}`);
+if (mode === "full" || mode === "all") {
+  run(`npx vite build -- -t reactive-full`);
+  run(`npx vite build -- -t html-full`);
+  run(`npx vite build -- -t whole-full`);
+  run(`npx tsc -p ${path("tsconfig.build.json ")} --emitDeclarationOnly --outDir ${path("dist/full")}`);
+}
+if (mode === "normal" || mode === "all") {
+  run(`npx vite build -- -t reactive`);
+  run(`npx vite build -- -t html`);
+  run(`npx vite build -- -t whole`);
+  run(`npx tsc -p ${path("tsconfig.build.json ")} --emitDeclarationOnly --outDir ${path("dist/normal")}`);
+}
