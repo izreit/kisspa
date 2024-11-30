5bfe47c77cdbe2a0eb1f2ec9e5d22cc734b6f169
izreit <737703+izreit@users.noreply.github.com>
2024-12-01 04:02:20 +0900

    test(sitekit): add forgotten expect() on a test

--- packages/sitekit/src/__tests__/weave.test.ts
+++ packages/sitekit/src/__tests__/weave.test.ts
@@ -25,8 +25,43 @@ function createEphemeralSitekitHandlers(root: string): EphemeralSitekitHandlers 
 describe("weave", () => {
   it("can handle simple html using JSX", async () => {
     const handlers = createEphemeralSitekitHandlers(__dirname);
-    const ctx = await createSitekitContext(handlers, join(__dirname, "fixtures/simple/.sitekit/"));
+    const ctx = await createSitekitContext({
+      configRoot: join(__dirname, "fixtures/simple/.sitekit/"),
+      handlers,
+    });
     await weave(ctx, "../pages/index.md");
-    console.log(handlers.written);
+
+    const { written } = handlers;
+    expect(Object.keys(written).sort()).toEqual([
+      'fixtures/simple/.sitekit/.wspace/index.doc.jsx',
+      'fixtures/simple/.sitekit/.wspace/index.html',
+      'fixtures/simple/.sitekit/.wspace/index.layout.jsx',
+    ]);
+
+    expect(written['fixtures/simple/.sitekit/.wspace/index.html']).toEqual(
+      '<!doctype html>\n' +
+      '<title>Top</title>\n' +
+      '\n' +
+      '<h1>Test Top</h1>\n' +
+      '\n' +
+      '<p>string in markdown.</p>\n' +
+      '<p><a href="./some.html">some</a></p>\n' +
+      '\n' +
+      '\n' +
+      '<div>Click to increment: <div data-sitekit-embed="L0" style="display:none"></div></div>\n' +
+      '<script type="module" src="./index.layout.jsx"></script>\n' +
+      '<script type="module" src="./index.doc.jsx"></script>\n',
+    );
+
+    expect(written['fixtures/simple/.sitekit/.wspace/index.layout.jsx']).toEqual(
+      'import { attach as __kisspa_attach__ } from "kisspa";\n' +
+      'import { TestComp } from "../theme/components/TestComp";\n' +
+      '\n' +
+      `__kisspa_attach__({ after: document.querySelector('[data-sitekit-embed="L0"]') }, <TestComp />);\n`,
+    );
+
+    expect(written['fixtures/simple/.sitekit/.wspace/index.doc.jsx']).toEqual(
+      '\n'
+    );
   });
 });
