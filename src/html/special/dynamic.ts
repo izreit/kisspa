335effb55ab8245a815c8b948acc9ab58885f991
izreit <737703+izreit@users.noreply.github.com>
2024-12-16 02:26:28 +0900

    fix(html)!: fix Dynamic to accept props as function

--- src/html/special/dynamic.ts
+++ src/html/special/dynamic.ts
@@ -4,16 +4,16 @@ import { jsx } from "../core/h";
 import type { Component } from "../core/types";
 
 export namespace Dynamic {
-  export type Props<T> = T & {
-    component?: () => Component<T>;
+  export type Props<T> = {
+    component: () => Component<T>;
+    props: () => T;
   }
 }
 
 export const Dynamic = createSpecial(<T>(actx: AssembleContext, props: Dynamic.Props<T>): Backing => {
   const base = createSimpleBacking("Dynamic");
   base.addDisposer_(autorun(() => {
-    if (props.component)
-      base.setBackings_([assemble(actx, jsx(props.component(), props))]);
+    base.setBackings_([assemble(actx, jsx(props.component(), props.props()))]);
   }));
   return base;
 });
