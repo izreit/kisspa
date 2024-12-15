27416b5f0b106bf3d71a457e6ce4abf39c55c6a6
izreit <737703+izreit@users.noreply.github.com>
2024-12-15 20:09:20 +0900

    test(html): add tests for <Show>, <Fragment>

---
+++ src/html/__tests__/fragment.test.tsx
@@ -0,0 +1,42 @@
+import { afterEach, beforeAll, describe, expect, it } from "vitest";
+import { observe } from "../../reactive";
+import { type Root, Fragment, createRoot, h } from "../index";
+
+describe("Fragment", () => {
+  let elem: HTMLElement;
+  let root: Root;
+
+  beforeAll(() => {
+    elem = document.createElement("div");
+    root = createRoot(elem);
+  });
+  afterEach(() => {
+    root.detach();
+  });
+
+  it("accepts multiple children", () => {
+    root.attach(
+      <>
+        <span>Foo</span>
+        <div>Bar</div>
+      </>
+    );
+    expect(elem.innerHTML).toBe("<span>Foo</span><div>Bar</div>");
+  });
+
+  it("can be nested", () => {
+    const [store, _setStore] = observe({ foo: 0 });
+    root.attach(
+      <>
+        <span>Foo</span>
+        <div>
+          <>
+            { store.foo }
+            <p>Zoo</p>
+          </>
+        </div>
+      </>
+    );
+    expect(elem.innerHTML).toBe("<span>Foo</span><div>0<p>Zoo</p></div>");
+  });
+});
