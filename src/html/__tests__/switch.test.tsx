ad429959f50b3fd57eb149bdb1cb53b44a77f960
izreit <737703+izreit@users.noreply.github.com>
2024-12-21 05:53:58 +0900

    fix(html)!: overhaul <Switch/>, remove fallback of <Switch/>

---
+++ src/html/__tests__/switch.test.tsx
@@ -0,0 +1,106 @@
+import { afterEach, beforeAll, describe, expect, it } from "vitest";
+import { observe } from "../../reactive";
+import { type JSXNode, type JSXNodeAsync, Match, type Root, Switch, createRoot, h } from "../index";
+
+describe("Switch", () => {
+  let elem: HTMLElement;
+  let root: Root;
+
+  beforeAll(() => {
+    elem = document.createElement("div");
+    root = createRoot(elem);
+  });
+  afterEach(() => {
+    root.detach();
+  });
+
+  it("selects Match", async () => {
+    const [store, setStore] = observe({ x: 4 as number | string});
+    root.attach(
+      <Switch>
+        <Match when={() => typeof store.x === "number" && 0 <= store.x && store.x < 10}>
+          <div>smallnum:{() => "" + store.x}</div>
+        </Match>
+        <Match when={() => typeof store.x === "string"}>
+          <div>str:{() => store.x}</div>
+        </Match>
+      </Switch>
+    );
+    expect(elem.innerHTML).toBe("<div>smallnum:4</div>");
+    setStore(s => s.x = "foo");
+    expect(elem.innerHTML).toBe("<div>str:foo</div>");
+    setStore(s => s.x = 100);
+    expect(elem.innerHTML).toBe("");
+  });
+
+  it("prefers preceded Match", async () => {
+    const [store, setStore] = observe({ x: 4 as number | string});
+    root.attach(
+      <Switch>
+        <Match when={() => typeof store.x === "number" && 0 <= store.x && store.x < 10}>
+          <div>smallnum:{() => "" + store.x}</div>
+        </Match>
+        <Match when={() => typeof store.x === "number" && 0 <= store.x && store.x < 10}>
+          <div>NEVERUSED</div>
+        </Match>
+        <Match when={() => typeof store.x === "string"}>
+          <div>str:{() => store.x}</div>
+        </Match>
+      </Switch>
+    );
+    expect(elem.innerHTML).toBe("<div>smallnum:4</div>");
+    setStore(s => s.x = "foo");
+    expect(elem.innerHTML).toBe("<div>str:foo</div>");
+    setStore(s => s.x = 100);
+    expect(elem.innerHTML).toBe("");
+  });
+
+  it("can provide type guard", async () => {
+    const [store, setStore] = observe({ x: 4 as number | string});
+    root.attach(
+      <Switch>
+        <Match when={() => (typeof store.x === "number" && 0 <= store.x && store.x < 10) && store.x} guarded>{x => (
+          <div>smallnum:{() => x() * 3}</div>
+        )}
+        </Match>
+        <Match when={() => (typeof store.x === "string") && store.x} guarded>{(val) => (
+          <div>str:{() => val().charCodeAt(0)}</div>
+        )}
+        </Match>
+      </Switch>
+    );
+    expect(elem.innerHTML).toBe("<div>smallnum:12</div>");
+    setStore(s => s.x = "foo");
+    expect(elem.innerHTML).toBe("<div>str:102</div>");
+    setStore(s => s.x = "g");
+    expect(elem.innerHTML).toBe("<div>str:103</div>");
+    setStore(s => s.x = 100);
+    expect(elem.innerHTML).toBe("");
+  });
+
+  it("fallbacks to the condition-less Match", async () => {
+    const [store, setStore] = observe({ x: 4 as number | string});
+    root.attach(
+      <Switch>
+        <Match when={() => (typeof store.x === "number" && 0 <= store.x && store.x < 10) && store.x} guarded>{x => (
+          <div>smallnum:{() => x() * 3}</div>
+        )}
+        </Match>
+        <Match when={() => (typeof store.x === "string") && store.x} guarded>{(val) => (
+          <div>str:{() => val().charCodeAt(0)}</div>
+        )}
+        </Match>
+        <Match>
+          <p>Fallback</p>
+        </Match>
+      </Switch>
+    );
+    expect(elem.innerHTML).toBe("<div>smallnum:12</div>");
+    setStore(s => s.x = 5);
+    expect(elem.innerHTML).toBe("<div>smallnum:15</div>");
+    setStore(s => s.x = 100);
+    expect(elem.innerHTML).toBe("<p>Fallback</p>");
+    setStore(s => s.x = "foo");
+    expect(elem.innerHTML).toBe("<div>str:102</div>");
+  });
+});
--- src/html/special/switch.ts
+++ src/html/special/switch.ts
@@ -1,32 +1,36 @@
 import { autorun, signal, watchProbe } from "../../reactive";
 import { type AssembleContext, type Backing, assemble, callAll, createSimpleBacking, createSpecial } from "../core/backing";
-import { jsx } from "../core/h";
-import type { JSXNode, PropChildren } from "../core/types";
-import { arrayify, isFunction, mapCoerce } from "../core/util";
+import type { PropChildren } from "../core/types";
+import { arrayify, mapCoerce } from "../core/util";
 import { createContextFun } from "./context";
-import { Show } from "./show";
 
 interface SwitchContextValue {
-  register_(cond: () => unknown): number;
-  active_(): number;
+  register_(cond: () => unknown, strict: boolean | null | undefined): [() => boolean, () => unknown];
   dispose_():void;
 }
 
 function createSwitchContextValue(): SwitchContextValue {
+  const notFalse = (v: unknown) => v !== false;
   const [activeIndex, setActiveIndex] = signal<number>(-1);
-  const stats: boolean[] = [];
+  const stats: unknown[] = [];
+  const judges: ((v: unknown) => unknown)[] = [];
   const disposers: (() => void)[] = [];
 
   return {
-    register_(cond) {
+    register_(cond, strict) {
       const idx = stats.push(false) - 1;
-      disposers.push(autorun(() => {
-        stats[idx] = !!cond();
-        setActiveIndex(stats.findIndex(v => v));
-      }));
-      return idx;
+      const [guardedValue, setGuardedValue] = signal<unknown>(null);
+      const judge = strict ? notFalse : Boolean;
+      judges[idx] = judge;
+      disposers.push(
+        watchProbe(cond, setGuardedValue, judge),
+        autorun(() => {
+          stats[idx] = cond();
+          setActiveIndex(stats.findIndex((v, i) => judges[i](v)));
+        })
+      );
+      return [() => activeIndex() === idx, guardedValue];
     },
-    active_: activeIndex,
     dispose_() { callAll(disposers); }
   };
 }
@@ -35,63 +39,55 @@ const { ProviderFun: switchContextProviderFun, use: useSwitchContext } = createC
 
 export namespace Switch {
   export interface Props {
-    fallback?: JSXNode;
     children?: PropChildren;
   }
 }
 
 export const Switch = createSpecial((actx: AssembleContext, props: Switch.Props): Backing => {
-  const { fallback, children: rawChildren } = props;
+  const { children: rawChildren } = props;
   const children = arrayify(rawChildren);
   const switchCtx = createSwitchContextValue();
 
-  // Equivalent to <SwitchContextProvider value={ctx}> {children} <Show when={...}>{fallback}</Show> </SwitchContextProvider>
-  const b = switchContextProviderFun(
-    actx,
-    {
-      value: switchCtx,
-      children: (fallback && rawChildren) ? [
-        ...children,
-        jsx(Show, { when: () => switchCtx.active_() === -1, children: fallback }),
-      ] : children
-    }
-  );
+  // Equivalent to <SwitchContextProvider value={ctx}> {children} </SwitchContextProvider>
+  const b = switchContextProviderFun(actx, { value: switchCtx, children });
+
   b.addDisposer_(switchCtx.dispose_);
   return b;
 });
 
 export namespace Match {
   export interface WhenProps {
-    when: () => boolean;
+    when?: () => boolean;
+    guarded?: false;
     children?: PropChildren;
   }
-  export interface GuardProps<T extends object> {
-    when: () => T | false;
-    children?: (v: T) => PropChildren;
+  export interface GuardProps<T> {
+    when: () => Exclude<T, boolean> | false;
+    guarded: true;
+    children?: (v: () => Exclude<T, boolean>) => PropChildren;
   }
-  export type Props<T extends object> = WhenProps | GuardProps<T>;
+  export type Props<T> = WhenProps | GuardProps<T>;
 }
 
-export const Match = createSpecial(<T extends object>(actx: AssembleContext, props: Match.Props<T>): Backing => {
-  const { when, children } = props;
-  let showing = false;
-  const switchCtx = useSwitchContext();
-  const index = switchCtx.register_(when);
-
+export const Match = createSpecial(<T>(actx: AssembleContext, props: Match.Props<T>): Backing => {
+  const { when, guarded, children } = props;
+  const [isActive, guardedValue] = useSwitchContext().register_(when || (() => true), guarded);
   const base = createSimpleBacking("Match");
 
-  base.addDisposer_(watchProbe(
-    () => (switchCtx.active_() === index),
-    toShow => {
+  let showing = false;
+  base.addDisposer_(
+    watchProbe(isActive, toShow => {
       showing = toShow;
       let bs: Backing[] | null | undefined;
       if (showing && children) {
-        const cs = isFunction(children) ? children(when() as T): children; // `as T` is valid since guard() is true here
+        const cs = guarded ?
+          children(guardedValue as (() => Exclude<T, boolean>)) : // 'as' is inevitable since it's provided by switchCtx...
+          children;
         bs = mapCoerce(cs, c => assemble(actx, c));
       }
       base.setBackings_(bs);
-    }
-  ));
+    })
+  );
 
   return base;
 });
