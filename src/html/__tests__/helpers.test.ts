af68b34d33ac0d4befdb02161ea64012eb7868e9
izreit <737703+izreit@users.noreply.github.com>
2025-01-18 14:09:11 +0900

    feat(html): add Prop<T> and deprop()

---
+++ src/html/__tests__/helpers.test.ts
@@ -0,0 +1,22 @@
+import { describe, expect, it } from "vitest";
+import { type Prop, deprop } from "../index";
+
+describe("helpers", () => {
+  describe("deprop", () => {
+    it("picks the value from literal Prop", () => {
+      const p: Prop<string> = "foo";
+      expect(deprop(p)).toBe("foo");
+    });
+
+    it("picks the value from function Prop", () => {
+      const p: Prop<string> = () => "zoo";
+      expect(deprop(p)).toBe("zoo");
+    });
+
+    it("accepts nullish", () => {
+      const p: Prop<string> | null = (Math.random() >= 0) ? null : (() => "zoo");
+      const v = deprop(p);
+      expect(v).toBe(null);
+    });
+  });
+});
---
+++ src/html/core/helpers.ts
@@ -0,0 +1,22 @@
+/**
+ * Utility type: either given type T or function returns T (i.e. `(() => T)`).
+ *
+ * Many props need to accept both static and dynamic values.
+ * For example, the `name` prop of the following:
+ *  - `<NameCard name="John" />`
+ *  - `<NameCard name={() => currentUesr.name} />
+ * So props are often typed as like `(string | (() => string))`.
+ *
+ * `Prop<T> is a helper for this.
+ * `Props<string>` can be used as `string | (() => string)`.
+ */
+export type Prop<T> = [T] extends [Function] ? never : (T | (() => T));
+
+/**
+ * Pick the value from given Prop<T>.
+ *
+ * @param p target Prop<T>.
+ */
+export function deprop<T>(p: Prop<T>): T {
+  return (typeof p === "function") ? p() : p;
+}
